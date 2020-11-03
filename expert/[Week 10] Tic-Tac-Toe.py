c9040507ca6afcf43ae65b617cc0b80f73f1cdea
Synthesizer <tunlaton11@gmail.com>
2020-11-03 14:48:52 +0700

    complete

--- expert/[Week 10] Tic-Tac-Toe.py
+++ expert/[Week 10] Tic-Tac-Toe.py
@@ -1,13 +1,74 @@
 """[Week 10] Tic-Tac-Toe"""
 
+def check_win(check_num):
+    """check winner"""
+    if check_num[0] == "X" and check_num[1] == "X" and check_num[2] == "X":
+        return True
+    elif check_num[3] == "X" and check_num[4] == "X" and check_num[5] == "X":
+        return True
+    elif check_num[6] == "X" and check_num[7] == "X" and check_num[8] == "X":
+        return True
+    elif check_num[0] == "X" and check_num[3] == "X" and check_num[6] == "X":
+        return True
+    elif check_num[1] == "X" and check_num[4] == "X" and check_num[7] == "X":
+        return True
+    elif check_num[2] == "X" and check_num[5] == "X" and check_num[8] == "X":
+        return True
+    elif check_num[0] == "X" and check_num[4] == "X" and check_num[8] == "X":
+        return True
+    elif check_num[2] == "X" and check_num[4] == "X" and check_num[6] == "X":
+        return True
+    else:
+        False
+
+def check_win_2(check_num):
+    """check winner"""
+    if check_num[0] == "O" and check_num[1] == "O" and check_num[2] == "O":
+        return True
+    elif check_num[3] == "O" and check_num[4] == "O" and check_num[5] == "O":
+        return True
+    elif check_num[6] == "O" and check_num[7] == "O" and check_num[8] == "O":
+        return True
+    elif check_num[0] == "O" and check_num[3] == "O" and check_num[6] == "O":
+        return True
+    elif check_num[1] == "O" and check_num[4] == "O" and check_num[7] == "O":
+        return True
+    elif check_num[2] == "O" and check_num[5] == "O" and check_num[8] == "O":
+        return True
+    elif check_num[0] == "O" and check_num[4] == "O" and check_num[8] == "O":
+        return True
+    elif check_num[2] == "O" and check_num[4] == "O" and check_num[6] == "O":
+        return True
+    else:
+        return False
 
 def main():
     """[Week 10] Tic-Tac-Toe"""
-    print("Welcome to OX!")
-    count = 1
+    print("Welcome to OX!\n")
     pos_array = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-    check_num = []
-    while True:
+    for i in range(len(pos_array)):
+        print("+-+-+-+")
+        for j in range(3):
+            print("|" + str(pos_array[i][j]), end='')
+        print("|")
+    print("+-+-+-+\n")
+
+    winner = ''
+    count = 1
+    is_win = ''
+    while is_win == '':
+        check_num = [num_j for num_i in pos_array for num_j in num_i]
+        if check_win(check_num) == True:
+            winner = 'The winner is X!!'
+            break
+
+        elif check_win_2(check_num) == True:
+            winner = 'The winner is O!!'
+            break
+        elif count == 10:
+            winner = 'Draw!!'
+            break
+
         if count % 2 != 0:
             print("It's X's turn!")
             mark = "X"
@@ -15,7 +76,6 @@ def main():
             print("It's O's turn!")
             mark = "O"
 
-        check_num = [num_j for num_i in pos_array for num_j in num_i]
         num = int(input("Please enter cell number (1-9) --> "))
         if not(1 <= num <= 9) or check_num[num - 1] == "X" or check_num[num - 1] == "O":
             continue
@@ -31,5 +91,6 @@ def main():
                 print("|")
             print("+-+-+-+\n")
             count += 1
+    print(winner)
 
 main()
--- expert/test.py
+++ expert/test.py
@@ -1,3 +1,21 @@
-x = [[1,2,3],['a','b']]
-check_num = [j for i in x for j in i]
-print(check_num)
+def check_win(check_num):
+    """check winner"""
+    if check_num[0] == "X" and check_num[1] == "X" and check_num[2] == "X":
+        return "The winner is X!!"
+    elif check_num[3] == "X" and check_num[4] == "X" and check_num[5] == "X":
+        return "The winner is X!!"
+    elif check_num[6] == "X" and check_num[7] == "X" and check_num[8] == "X":
+        return "The winner is X!!"
+    elif check_num[0] == "X" and check_num[3] == "X" and check_num[6] == "X":
+        return "The winner is X!!"
+    elif check_num[1] == "X" and check_num[4] == "X" and check_num[7] == "X":
+        return "The winner is X!!"
+    elif check_num[2] == "X" and check_num[5] == "X" and check_num[8] == "X":
+        return "The winner is X!!"
+    elif check_num[0] == "X" and check_num[4] == "X" and check_num[8] == "X":
+        return "The winner is X!!"
+    elif check_num[2] == "X" and check_num[4] == "X" and check_num[6] == "X":
+        return "The winner is X!!"
+
+check_num = ["X","X","O","O","X",6,7,"O","X"]
+print(check_win(check_num))
