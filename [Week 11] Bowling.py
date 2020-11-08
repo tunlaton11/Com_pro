1bf912f8a2f8747366137fe6ef5d30aeb1ab8875
Synthesizer <tunlaton11@gmail.com>
2020-11-08 22:09:06 +0700

    commit

---
+++ [Week 11] Bowling.py
@@ -0,0 +1,45 @@
+"""[Week 11] Bowling"""
+
+
+def bowling_score(line):
+    """[Week 11] Bowling"""
+    score = 0
+    for i in range(len(line)):
+        if line[i] == 'X':
+            line[i] = 10
+        elif line[i] == '-':
+            line[i] = 0
+        elif line[i] == '/':
+            continue
+        else:
+            line[i] = int(line[i])
+    for j in range(len(line)):
+        ball = line[j]
+        if len(line) - 3 <= j:
+            if ball == '/':
+                score += (10 - line[j - 1])
+            else:
+                score += ball
+            continue
+        elif ball == 10:
+            score += ball
+            score += line[j + 1]
+            if line[j + 2] == '/':
+                score += (10 - line[j + 1])
+            else:
+                score += line[j + 2]
+        elif ball == '/':
+            score += (10 - line[j - 1])
+            score += line[j + 1]
+        else:
+            score += ball
+    return score
+
+
+def main():
+    """[Week 11] Bowling"""
+    line = list(''.join(input().split()))
+    print(bowling_score(line))
+
+
+main()
--- [Week 11] Descendants of the Sun.py
+++ [Week 11] Descendants of the Sun.py
@@ -4,35 +4,32 @@
 import math
 
 
-def direct(lat1, lat2, lon1, lon2):
+def check_direct(lat1, lat2, lon1, lon2): #check direction of lat and lon
     """[Week 11] Descendants of the Sun"""
-    dLon = (lon2 - lon1)
-    y_num = math.sin(dLon) * math.cos(lat2)
-    x_num = math.cos(lat1) * math.sin(lat2) - math.sin(lat1) * math.cos(lat2) * math.cos(dLon)
-    bearing = int((math.atan2(y_num, x_num) * 180 / math.pi + 360) % 360)
-    if bearing == 0:
+    if lon2 == lon1 and lat2 > lat1:
         direct = "N"
-    elif 0 < bearing < 90:
+    elif lon2 > lon1 and lat2 > lat1:
         direct = "NE"
-    elif bearing == 90:
+    elif lon2 > lon1 and lat2 == lat1:
         direct = "E"
-    elif 90 < bearing < 180:
+    elif lon2 > lon1 and lat2 < lat1:
         direct = "SE"
-    elif bearing == 180:
+    elif lon2 == lon1 and lat2 < lat1:
         direct = "S"
-    elif 180 < bearing < 270:
+    elif lon2 < lon1 and lat2 < lat1:
         direct = "SW"
-    elif bearing == 270:
+    elif lon2 < lon1 and lat2 == lat1:
         direct = "W"
-    elif 270 < bearing < 360:
+    elif lon2 < lon1 and lat2 > lat1:
         direct = "NW"
     return direct
 
 
-def result(lat1, lat2, lon1, lon2):
+def check_result(lat1, lat2, lon1, lon2):
     """[Week 11] Descendants of the Sun"""
     rad = 6378.137
-    result = 2 * rad * math.asin(math.sqrt(math.sin((lat2 - lat1) / 2) ** 2 + math.cos(lat1)*math.cos(lat2)*math.sin((lon2 - lon1) / 2) ** 2))
+    result = 2 * rad * math.asin(math.sqrt(math.sin((lat2 - lat1) / 2) ** 2 + \
+             math.cos(lat1)*math.cos(lat2)*math.sin((lon2 - lon1) / 2) ** 2))
     return "%.2f" % result + "km"
 
 
@@ -50,13 +47,13 @@ def main():
             data.append(tuple(sub))
             sub = []
 
-
         for i in range(num):
             lat1 = math.radians(data[i][0])
             lon1 = math.radians(data[i][1])
             lat2 = math.radians(data[i+1][0])
             lon2 = math.radians(data[i+1][1])
-            print("#" + str(i + 1) + " Distance:", result(lat1, lat2, lon1, lon2), "Direction:", direct(lat1, lat2, lon1, lon2))
+            print("#" + str(i + 1) + " Distance:", check_result(lat1, lat2, lon1, lon2),
+                  "Direction:", check_direct(lat1, lat2, lon1, lon2))
 
 
 main()
